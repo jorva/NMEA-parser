0b914ceb1ca967fc948ed91560c0aee3dcb7d6ae
Mika Jarvenpaa <mika.o.jarvenpaa@gmail.com>
2015-09-10 23:37:01 +0300

    Initial class skeleton for SerialPort

---
+++ SerialPort.h
@@ -0,0 +1,28 @@
+#ifndef SERIAL_PORT_H_INCLUDED
+#define SERIAL_PORT_H_INCLUDED
+
+#include <stdio>
+
+
+class SerialPort
+{
+	public:
+	
+		//c-tor
+		SerialPort();
+		
+		//open connection
+		
+		//read data
+		
+		//write data
+		
+		//close
+		~SerialPort();
+		
+	
+	private:
+	
+};
+
+#endif //SERIAL_PORT_H_INCLUDED
